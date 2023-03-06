ff86074acdd6037dcafd363291bcfe3a00660e2f
Srdjan Petrovic <s.petrovic852@gmail.com>
2023-03-07 00:06:42 +0100

    initial commit

---
+++ OOPIntro/InterestEarningAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPIntro
+{
+    internal class InterestEarningAccount : BankAccount
+    {
+        public InterestEarningAccount(string name, decimal initialBalance) : base(name, initialBalance)
+        {
+
+        }
+    }
+}
---
+++ OOPIntro/Transaction.cs
@@ -0,0 +1,16 @@
+﻿namespace OOPIntro
+{
+    internal class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public  string Note { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            Amount = amount;
+            Date = date;
+            Note = note;
+        }
+    }
+}
